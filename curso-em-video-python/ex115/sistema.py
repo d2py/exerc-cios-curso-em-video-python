15205f667cb7ff5aea121a98b8938ed63af4e563
DIEGO DIAS <127305708+d2py@users.noreply.github.com>
2024-04-12 12:33:17 -0300

    atualização

--- curso-em-video-python/ex115/sistema.py
+++ curso-em-video-python/ex115/sistema.py
@@ -1,11 +1,18 @@
 from lib.interface import *
+from lib.arquivo import *
 from time import sleep
 
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
-    resposta = menu(['Criar Arquivo', 'Cadatrar Pessoas', 'Listar Pessoas', 'Sair do Sistema'])
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        print('Opcao 1')
+        # Opcão de listar o conteudo de um arquivo!
+        lerArquivo(arq)
+
     elif resposta == 2:
         print('Opcao 2')
     elif resposta == 3:
@@ -14,3 +21,6 @@ while True:
     else:
         print('\033[31mERRO! Digite uma opção válida\033[m')
     sleep(2)
+
+
+
